9bdee13f1d81bc8d79d6fa0aea39584809353f65
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-08 18:45:32 -0600

    guardado en distintos tipos de archivos

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -11,26 +11,13 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n\n')
-        # print(response.status, response.headers)
         title = response.xpath('//h1/a/text()').get() # Se le pone get para obtener el titulo, ya que solo es uno
-        print(f'Titulo: {title}')
-        
-        print('\n\n')
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').extract()
-        print('Citas: ')
-        for quote in quotes:
-            print(f'- {quote}')
-        print('\n\n')
-        
 
-        print('\n\n')
         top_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').extract()
-        print('El top 10 de las citas es: ')
-        for tag in top_tags:
-            print(f'- {tag}')
-        print('\n\n')
 
-        print('\n\n\n')
-        print('*' * 10)
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_tags': top_tags,
+        }
