ea273d89cde0a7c1918902c44b0d4d5e240d41c2
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-09 23:30:41 -0600

    challenge done

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -4,6 +4,7 @@ import scrapy
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
 # Next Page Button = //ul[@class="pager"]//li[@class="next"]/a/@href
+# Author = //span/small[@class="author" and @itemprop="author"]/text()
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes' # Nombre unico con el cual scrapy se va a referir a este spider dentro del proyecto, no es repetible
@@ -24,27 +25,41 @@ class QuotesSpider(scrapy.Spider):
                 },
             },
         },
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['oscarbucio2001@gmail.com'],
+        'ROBOTSTXT_OBEY': False,
+        'USER_AGENT': 'Automaton',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
+            authors = kwargs['authors']
+
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').extract()) # Se va a estar llenando cada vez dependiendo si existe o no next_page_button_link por el callback que se manda
+        authors.extend(response.xpath('//span/small[@class="author" and @itemprop="author"]/text()').extract())
 
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
+
         else:
-            yield {
-                'quotes': quotes
-            } # Se regresa ya al final si la validación del next_áge_button_link fue falsa
+            for i, j in zip(quotes, authors):
+                yield {
+                    'quote': i,
+                    'author': j
+                }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get() # Se le pone get para obtener el titulo, ya que solo es uno
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').extract()
 
         top_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').extract()
+
+        authors = response.xpath('//span/small[@class="author" and @itemprop="author"]/text()').extract()
 
         top = getattr(self, 'top', None) # Se le pasa la instancia self, se busca 'top', si existe se guarda dentro de la variable top si el atributo no existe el resultado será None 
         if top:
@@ -58,4 +73,4 @@ class QuotesSpider(scrapy.Spider):
 
         next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
