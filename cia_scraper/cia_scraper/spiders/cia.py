58ea33cf3ef52549930e1fd623b73d955b220da2
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-24 14:22:09 -0600

    done

--- cia_scraper/cia_scraper/spiders/cia.py
+++ cia_scraper/cia_scraper/spiders/cia.py
@@ -2,6 +2,8 @@ import scrapy
 
 # XPath
 # Links = //a[starts-with(@href, "collection") and (parent::h3 | parent::h2)]/@href
+# Title = //h1[@class="documentFirstHeading"]/text()
+# Paragraph = //div[@class="field-item even"]/p[not(@class)]/text()
 
 class SpiderCIA(scrapy.Spider):
     name = 'cia'
@@ -32,7 +34,15 @@ class SpiderCIA(scrapy.Spider):
         links_desclassified = response.xpath('//a[starts-with(@href, "collection") and (parent::h3 | parent::h2)]/@href').extract()
 
         for link in links_desclassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url': response.urljoin(link)})
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph,
+        }
